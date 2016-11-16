345349edde6815a53ace0280ea0252180eb849a0
zhengkai <zkawaken@gmail.com>
2016-11-16 14:43:25 -0600

    functional prototype

--- js/mwh.js
+++ js/mwh.js
@@ -2,7 +2,7 @@ var quarterBeatLength;
 var barLength;
 var inputTimes;
 var rhythm;
-var notes;
+var rhythmBars;
 
 var VF = Vex.Flow;
 
@@ -10,6 +10,10 @@ var recording;
 
 var tempoSlider = $('#tempo_range');
 var resolutionSelector = $('#resolution_selector');
+var precisionSelector = $('#precision_selector');
+var precision;
+
+var restsString;
 
 $(document).ready(function () {
     $('#tempo_input').keyup(function (event) {
@@ -31,8 +35,9 @@ $(document).ready(function () {
     initMetronome();
     tempoSlider.slider('setValue', tempo, true, true);
     resolutionSelector.val('eighth').trigger('change');
+    precisionSelector.val('eighth').trigger('change');
 
-    drawNotation();
+    drawStaves();
 });
 
 tempoSlider.slider({
@@ -54,6 +59,23 @@ tempoSlider.change(function () {
 resolutionSelector.change(function () {
     var noteResolutionValue = $(this).val();
     setNoteResolution(noteResolutionValue);
+});
+
+precisionSelector.change(function () {
+    var precisionValue = $(this).val();
+    switch (precisionValue) {
+        case 'sixteenth':
+            precision = 16;
+            break;
+        case 'eighth':
+            precision = 8;
+            break;
+        case 'quarter':
+            precision = 4;
+            break;
+        default:
+    }
+    drawStaves();
 });
 
 $('#play_metronome').click(function () {
@@ -89,13 +111,18 @@ function endRecording() {
             calculateRhythm();
             console.log(rhythm.toString());
             generateNotes();
-            drawNotation();
+            console.log(rhythmBars.toString());
+            drawNotation(rhythmBars);
         }
     }
 }
 
+function diff(time1, time2) {
+    return Math.abs(time1 - time2);
+}
+
 function goodTiming(time1, time2, threshold) {
-    return Math.abs(time1 - time2) < threshold;
+    return diff(time1, time2) < threshold;
 }
 
 function calculateRhythm() {
@@ -107,19 +134,19 @@ function calculateRhythm() {
     var endTime = lastBeatTime;
     var timePerBeat;
 
-    switch (resolutionSelector.val()) {
-        case 'sixteenth':
+    switch (precision) {
+        case 16:
             timePerBeat = 60 / tempo / 4 * 1000;
             break;
-        case 'eighth':
+        case 8:
             timePerBeat = 60 / tempo / 2 * 1000;
             break;
-        case 'quarter':
+        case 4:
             timePerBeat = 60 / tempo * 1000;
             break;
         default:
     }
-    var threshold = timePerBeat * 0.2;
+    var threshold = timePerBeat * 0.4;
 
     console.log("startTime = " + startTime);
     console.log("endTime = " + endTime);
@@ -131,82 +158,150 @@ function calculateRhythm() {
         beats.push(time);
     }
 
-    for (var inputI = 0, beatI = 0; inputI < inputTimes.length && beatI < beats.length;) {
-        var inputTime = inputTimes[inputI];
-        var beat = beats[beatI];
+    // find the nearest beat for each input
 
-        console.log("input = " + inputI + " " + inputTime);
-        console.log("beat  = " + beatI + " " + beat);
+    // handle the first input specially,
+    // because it may start before any beats,
+    // i.e. its time may be less than any beats
+    if (inputTimes[0] < beats[0]) {
+        if (goodTiming(inputTimes[0], beats[0], threshold)) {
+            rhythm.push(0);
+        }
+    }
 
-        if (goodTiming(inputTime, beat, threshold)) {
-            rhythm.push(beatI);
-            inputI++;
-            beatI++;
-        } else {
-            // for moving beatI: (and for moving inputI vice versa)
-            // case 1:
-            //   before:
-            //     input: x         X             x
-            //     beats: o   o   A   o   o   o   o   o
-            //   after:
-            //     input: x         X             x
-            //     beats: o   o   o   A   o   o   o   o
-            //
-            // case 2:
-            //   before:
-            //     input: x         x            X
-            //     beats: o   o   o   A   o   o   o   o
-            //   after:
-            //     input: x         x            X
-            //     beats: o   o   o   o   o   A   o   o
-            if (inputTime < beat) {
-                inputI++;
-                if (inputTimes[inputI] < beat) {
-                    while (inputTimes[inputI] < beat) {
-                        inputI++;
-                    }
-                    inputI--;
+    for (var beatI = 0, inputI = 0; inputI < inputTimes.length; inputI++) {
+        // find the nearest beat that less than input
+        //   for:
+        //     input: x                 X        x
+        //     beats: o   o   o   o   o   o   o   o
+        //   find:
+        //     input: x                 X        x
+        //     beats: o   o   o   o   A   B   o   o
+        // and then determine, of A and B, which is nearer to X
+        // i.e. compare abs(A-X) and abs(B-X).
+
+        if (beats[beatI] < inputTimes[inputI]) {
+            if (beats[beatI + 1] < inputTimes[inputI]) {
+                while (beats[beatI + 1] < inputTimes[inputI]) {
+                    beatI++;
                 }
-            } else {
-                beatI++;
-                if (inputTime > beats[beatI]) {
-                    while (inputTime > beats[beatI]) {
-                        beatI++;
-                    }
-                    beatI--;
-                }
+            }
+
+            var diffA = diff(beats[beatI], inputTimes[inputI]);
+            var diffB = diff(beats[beatI + 1], inputTimes[inputI]);
+
+            var nearestBeatI = diffA < diffB ? beatI : beatI + 1;
+            if (rhythm[rhythm.length - 1] !== nearestBeatI) {
+                rhythm.push(nearestBeatI);
+                console.log("nearest beat: " + nearestBeatI);
+                console.log("difference  : " + diff(beats[nearestBeatI], inputTimes[inputI]));
             }
         }
     }
+
+    // // find the those beats whose difference < threshold
+    // for (var beatI = 0, inputI = 0; beatI < beats.length && inputI < inputTimes.length;) {
+    //     var beat = beats[beatI];
+    //     var inputTime = inputTimes[inputI];
+    //
+    //     console.log("beat  = " + beatI + " " + beat);
+    //     console.log("input = " + inputI + " " + inputTime);
+    //
+    //     if (goodTiming(beat, inputTime, threshold)) {
+    //         rhythm.push(beatI);
+    //         console.log("timing beat: " + beatI);
+    //         console.log("difference : " + diff(beat, inputTime));
+    //         beatI++;
+    //         inputI++;
+    //     } else {
+    //         // if beat < input, then move beatI, else move inputI
+    //         // for moving beatI (and for moving inputI vice versa):
+    //         // case 1:
+    //         //   before:
+    //         //     input: x         X             x
+    //         //     beats: o   o   A   o   o   o   o   o
+    //         //   after:
+    //         //     input: x         X             x
+    //         //     beats: o   o   o   A   o   o   o   o
+    //         //
+    //         // case 2:
+    //         //   before:
+    //         //     input: x         x            X
+    //         //     beats: o   o   o   A   o   o   o   o
+    //         //   after:
+    //         //     input: x         x            X
+    //         //     beats: o   o   o   o   o   A   o   o
+    //         if (beat < inputTime) {
+    //             if (beats[beatI + 1] < inputTime) {
+    //                 while (beats[beatI + 1] < inputTime) {
+    //                     beatI++;
+    //                 }
+    //             }
+    //         } else {
+    //             if (inputTimes[inputI + 1] < beat) {
+    //                 while (inputTimes[inputI + 1] < beat) {
+    //                     inputI++;
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
 }
 
 function generateNotes() {
-    notes = [];
+    rhythmBars = [];
 
+    var bar = [];
+    var barI = 0;
+
+    for (var rhythmI = 0; rhythmI < rhythm.length; rhythmI++) {
+        var beat = rhythm[rhythmI];
+        console.log("beat: " + beat);
+
+        if (beat >= (barI + 1) * precision) {
+            if (bar.length === 0) {
+                rhythmBars.push(restsString);
+            } else {
+                rhythmBars.push(generateBar(bar));
+                bar = [];
+            }
+
+            barI++;
+        }
+
+        bar.push(beat % precision);
+    }
+
+    rhythmBars.push(generateBar(bar));
 }
 
-function generateNotesInBar(rhythmInBar) {
-    var bar = [];
+function makeBar(bar) {
+    bar[0] = bar[0].slice(0, 2) + '/' + precision + bar[0].slice(2);
+    return bar.join(',');
+}
 
-    for (var ni = 0, ri = 0; ri < rhythmInBar.length; ri++) {
-        if (rhythmInBar[ri] > notes.length * 8) {
+function generateBar(bar) {
+    console.log("bar: " + bar.toString());
+    // 'B4/16/r, B4/r, B4/r, B4/r, B4/r, B4/r, B4/r, B4/r, B4/r, B4/r, B4/r, B4/r, B4/r, B4/r, B4/r, B4/r'
+    var rhythmBar = [];
 
+    for (var ni = 0, ri = 0; ri < bar.length; ri++) {
+        for (var i = ni; i < bar[ri]; i++) {
+            rhythmBar.push('B4/r');
         }
-
-        for (var i = ni; i < rhythmInBar[ri]; i++) {
-            notes.push(new VF.StaveNote({clef: "treble", keys: ["b/4"], duration: "8r"}));
-        }
-        ni = rhythmInBar[ri] + 1;
-        notes.push(new VF.StaveNote({clef: "treble", keys: ["c/4"], duration: "8"}));
+        ni = bar[ri] + 1;
+        rhythmBar.push('G4');
     }
 
-    if (rhythmInBar.length != 0) {
-        for (i = rhythmInBar[rhythmInBar.length - 1]; i < 8; i++) {
-            notes.push(new VF.StaveNote({clef: "treble", keys: ["b/4"], duration: "8r"}));
+    if (bar.length != 0) {
+        for (i = bar[bar.length - 1] + 1; i < precision; i++) {
+            rhythmBar.push('B4/r');
         }
     }
 
-    return bar;
+    var rhythmBarString = makeBar(rhythmBar);
+    console.log("rhythmBar: " + rhythmBarString);
+    return rhythmBarString;
 }
 
 function recordTimePoint() {
@@ -286,8 +381,7 @@ $('#right').mouseenter(function () {
     }
 });
 
-// draw notation
-var registry;
+// notation position
 var vf;
 var x, y;
 function makeSystem(width) {
@@ -296,12 +390,19 @@ function makeSystem(width) {
     return system;
 }
 
-function concat(a, b) {
-    return a.concat(b);
+function drawStaves() {
+    var rests = [];
+    for (var i = 0; i < precision; i++) {
+        rests.push('B4/r');
+    }
+    restsString = makeBar(rests);
+    drawNotation([restsString, restsString, restsString]);
 }
 
-function drawNotation() {
-    registry = new VF.Registry();
+function drawNotation(bars) {
+    $('#notation').empty();
+
+    var registry = new VF.Registry();
     VF.Registry.enableDefaultRegistry(registry);
 
     vf = new VF.Factory({
@@ -314,88 +415,38 @@ function drawNotation() {
     });
 
     var score = vf.EasyScore({throwOnError: true});
-    score.set({time: '3/4'});
+    score.set({time: '4/4'});
 
     var voice = score.voice.bind(score);
     var notes = score.notes.bind(score);
     var beam = score.beam.bind(score);
 
-    x = 120;
-    y = 80;
+    x = 0;
+    y = 0;
+    var barLength = 40 + precision * 20;
 
-    /*  Measure 1 */
-    var system = makeSystem(220);
-    system.addStave({
-        voices: [
-            voice([
-                notes('D5/q'),
-                beam(notes('G4/8, A4, B4, C5', {stem: "up"}))
-            ].reduce(concat))
-        ]
-    })
-        .addClef('treble')
-        .addKeySignature('C')
-        .addTimeSignature('4/4');
+    if (bars[0]) {
+        var system = makeSystem(barLength + 60);
+        var stave = new VF.Stave();
+        system.addStave({voices: [voice(notes(bars[0]))]})
+            .addClef('treble').addKeySignature('C').addTimeSignature('4/4');
+    }
+    if (bars[1]) {
+        system = makeSystem(barLength);
+        system.addStave({voices: [voice(notes(bars[1]))]});
+    }
+    if (bars[2]) {
+        system = makeSystem(barLength);
+        system.addStave({voices: [voice(notes(bars[2]))]});
+    }
 
-    /*  Measure 2 */
-    system = makeSystem(150);
-    system.addStave({voices: [voice(notes('D5/q, G4, G4'))]});
-
-    /*  Measure 3 */
-    system = makeSystem(150);
-    system.addStave({
-        voices: [
-            voice([
-                notes('E5/q'),
-                beam(notes('C5/8, D5, E5, F5', {stem: "down"}))
-            ].reduce(concat))
-        ]
-    });
-
-    /*  Measure 4 */
-    system = makeSystem(150);
-    system.addStave({voices: [voice(notes('G5/q, G4, G4'))]});
-
-    /*  Measure 5 */
-    system = makeSystem(150);
-    system.addStave({
-        voices: [
-            voice([
-                notes('C5/q'),
-                beam(notes('D5/8, C5, B4, A4', {stem: "down"}))
-            ].reduce(concat))
-        ]
-    });
-
-    /*  Measure 6 */
-    system = makeSystem(150);
-    system.addStave({
-        voices: [
-            voice([
-                notes('B5/q'),
-                beam(notes('C5/8, B4, A4, G4', {stem: "up"}))
-            ].reduce(concat))
-        ]
-    });
-
-    /*  Measure 7 (New system) */
-    x = 20;
-    y += 120;
-
-    system = makeSystem(220);
-    system.addStave({
-        voices: [
-            voice([
-                notes('F4/q'),
-                beam(notes('G4/8, A4, B4, G4', {stem: "up"}))
-            ].reduce(concat))
-        ]
-    }).addClef('treble').addKeySignature('C');
-
-    /*  Measure 8 */
-    system = makeSystem(180);
-
-    system.addStave({voices: [voice(notes('A4/h.'))]});
+    // new line
+    // x = 0;
+    // y += 120;
+    //
+    // system = makeSystem(barLength);
+    // system.addStave({voices: [voice(notes(bars[3]))]})
+    //     .addClef('treble').addKeySignature('C');
 
     vf.draw();
 }
